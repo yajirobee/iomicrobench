6500497c7d592b4f16ba883c3c3067ebf932ec95
keisuke <secret.heart.ks@gmail.com>
2013-10-19 05:47:09 +0900

    trivial change

--- drawreadgraph.py
+++ drawreadgraph.py
@@ -4,8 +4,11 @@ import sys, os, sqlite3
 from plotutil import gpinit, query2gds
 
 slide = False
+slide = 1
 
-cols = ["mbps", "iops", "latency"]
+cols = [{"name": "mbps", "title": "Throughput (MB/s)", "ylabel": "MB/s"},
+        {"name": "iops", "title": "Throughput (IOPS)", "ylabel": "IOPS"},
+        {"name": "latency", "title": "Access latency", "ylabel": "latency [ns]"}]
 units = {"elapsed" : "[us]",
          "mbps" : "[MB/s]",
          "iops" : "",
@@ -34,19 +37,19 @@ def plot_iosize_spec(dbpath, terminaltype = "png"):
     gp.xlabel("I/O size [B]")
     gp('set format x "%.0s%cB"')
     for col in cols:
-        gp('set title "{0}"'.format(col))
-        gp.ylabel("{0} {1}".format(col, units[col]))
-        if col == "mbps" or col == "latency":
+        gp('set title "{0}"'.format(col["title"]))
+        gp.ylabel(col["ylabel"])
+        if col["name"] == "mbps" or col["name"] == "latency":
             gp('set key left top')
         else:
             gp('set key right top')
-        figpath = "{0}_{1}_xiosize.{2}".format(fpath, col, terminaltype)
+        figpath = "{0}_{1}_xiosize.{2}".format(fpath, col["name"], terminaltype)
         gp('set output "{0}"'.format(figpath))
         gds = []
         for tbl, nth in zip(tables, nthreadlistlist):
             query = ("select iosize,avg({0}) from {1} "
-                     "where nthread={{nthread}} and iosize <= 128 * 1024 "
-                     "group by iosize,nthread".format(col, tbl))
+                     "where nthread={{nthread}}"
+                     "group by iosize,nthread".format(col["name"], tbl))
             gds.extend(query2gds(conn, query, nthread = nth,
                                  title = "{0} {1} = {{{1}}}".format(tbl, "nthread"),
                                  with_ = "linespoints"))
@@ -67,7 +70,7 @@ def plot_nthread_spec(dbpath, terminaltype = "png"):
             gp('set termoption font "Times-Roman,28"')
             plotprefdict = {"with_" : "linespoints lt 1 lw 6" }
         elif "png" == terminaltype:
-            gp('set termoption font "Times-Roman,14"')
+            gp('set termoption font "Times-Roman,18"')
             plotprefdict = {"with_" : "linespoints lw 2"}
     else:
         plotprefdict = {"with_" : "linespoints" }
@@ -76,18 +79,18 @@ def plot_nthread_spec(dbpath, terminaltype = "png"):
                       for tbl in tables]
     gp.xlabel("nthread")
     for col in cols:
-        gp('set title "{0}"'.format(col))
-        gp.ylabel("{0} {1}".format(col, units[col]))
-        if col == "latency":
+        gp('set title "{0}"'.format(col["title"]))
+        gp.ylabel(col["ylabel"])
+        if col["name"] == "latency":
             gp('set key left top')
         else:
             gp('set key right top')
-        figpath = "{0}_{1}_xnthread.{2}".format(fpath, col, terminaltype)
+        figpath = "{0}_{1}_xnthread.{2}".format(fpath, col["name"], terminaltype)
         gp('set output "{0}"'.format(figpath))
         gds = []
         for tbl, ios in zip(tables, iosizelistlist):
             query = ("select nthread,avg({0}) from {1} where iosize={{iosize}} "
-                     "group by iosize,nthread".format(col, tbl))
+                     "group by iosize,nthread".format(col["name"], tbl))
             gds.extend(query2gds(conn, query, iosize = ios,
                                  title = "{0} {1} = {{{1}}}".format(tbl, "iosize"),
                                  **plotprefdict))
@@ -110,5 +113,5 @@ if __name__ == "__main__":
         sys.stdout.write("wrong terminal type\n")
         sys.exit(1)
 
-    plot_iosize_spec(dbpath, terminaltype)
-    #plot_nthread_spec(dbpath, terminaltype)
+    #plot_iosize_spec(dbpath, terminaltype)
+    plot_nthread_spec(dbpath, terminaltype)
